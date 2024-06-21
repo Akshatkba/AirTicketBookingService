8e98dcc16250ed9a2017279cf6bbd4997719441f
Akshat Bansal <akshatkba24@gmail.com>
2024-06-21 22:03:09 +0530

    first commit: basic setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const { PORT } = require('./config/serverconfig');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+
+const setupAndStartServer = () => {
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`Server started on port: ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter: true});
+        }
+    })
+}
+
+setupAndStartServer();
